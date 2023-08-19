3463953e61c2fea77389c1fee84d0f56796210e3
limzengkai96 <limzengkai96@gmail.com>
2023-08-19 19:09:44 +0800

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { BrowserRouter,Route } from 'react-router-dom';
+
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/navbar' element={<Navbar />}></Route>
-      
-    </BrowserRouter>
-
+    <div>
+      <Home/>
+      <Navbar/>
+    </div>
   );
 }
 
